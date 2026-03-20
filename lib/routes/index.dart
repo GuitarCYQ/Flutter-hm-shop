c3e4b2ee4f55a07fa7e99317c60d8f50731d4f0e
GuitarCYQ <1159543426@qq.com>
2026-03-20 10:11:20 +0800

    搭建基础路由和page组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,18 @@
+// 管理路由
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+// 返回app根级组件
+Widget getRouteWidget() {
+  return MaterialApp(
+    // 命名路由
+    initialRoute: '/',
+    routes: getRootRoutes(),
+  );
+}
+
+// 返回该app 的路由配置
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {'/': (context) => MainPage(), '/login': (context) => LoginPage()};
+}
