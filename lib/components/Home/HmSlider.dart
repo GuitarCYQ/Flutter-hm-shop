3d39b6e1e82f413ce8c7cf14c4efe324080b061b
GuitarCYQ <1159543426@qq.com>
2026-03-24 10:52:49 +0800

    实现轮播图效果渲染

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,7 +1,10 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  const HmSlider({super.key});
+  final List<BannerItem> bannerList;
+  const HmSlider({super.key, required this.bannerList});
 
   @override
   State<HmSlider> createState() => _HmSliderState();
@@ -9,12 +12,37 @@ class HmSlider extends StatefulWidget {
 
 class _HmSliderState extends State<HmSlider> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: Text('轮播图', style: TextStyle(color: Colors.white, fontSize: 20)),
-    );
+    Widget _getSlider() {
+      // 在Flutter 中获取屏幕宽度的方法
+      final double screenWidth = MediaQuery.of(context).size.width; // 屏幕宽度
+
+      // 返回轮播图插件
+      // 根据数据渲染的不同的轮播选项
+      return CarouselSlider(
+        items: List.generate(widget.bannerList.length, (int index) {
+          return Image.network(
+            widget.bannerList[index].imgUrl,
+            fit: BoxFit.cover, // 图片全覆盖
+            width: screenWidth, // 获取了屏幕的宽度
+          );
+        }),
+        options: CarouselOptions(
+          viewportFraction: 1, // 1：轮播图片占满全屏
+          autoPlay: true, // 自动播放
+          height: 300, // 高度
+          autoPlayInterval: Duration(seconds: 5), // 轮播自动播放间隔时间
+        ),
+      );
+    }
+
+    // Stack -> 轮播图 搜索框 指示灯导航
+    return Stack(children: [_getSlider()]);
   }
 }
